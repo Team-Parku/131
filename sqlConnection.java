acfc6e85ff09f3948c497a1eba03e5efe96f40e0
FeevaTime <feeva.time@gmail.com>
2015-05-05 15:05:54 -0700

    Create sqlConnection.java

---
+++ sqlConnection.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class sqlConnection {
+	Connection conn = null;
+	public static Connection dbConnector()
+	{
+		try{
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Tuff Genius\\Documents\\SQlite\\AdminData.sqlite");
+		    JOptionPane.showMessageDialog(null, "Connection Successful!");
+			return conn;
+		}
+		catch(Exception e){
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+	}
+}
