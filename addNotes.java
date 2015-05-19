e92e6a6ff1281db0926ca9c6ace72800c2d519b2
Matt <myamamoto10@gmail.com>
2015-05-19 12:19:20 -0800

    Hellow there
    BOO

--- addNotes.java
+++ addNotes.java
@@ -28,6 +28,8 @@ class addNotes extends JPanel {
    private String date = "";
    private String noteEntered = "";
    private static String[] parkList ={"Carnegie", "Clay Pit", "Heber Dunes", "Hollister Hills", "Hungry Valley", "Oceano Dunes", "Ocohillo Wells", "Prairie City"};
+   //private static String[] monthList = {"1","2","3","4","5","6","7","8","9","10","11","12"};
+   
    private JComboBox<String> parkSelect;
    //private JComboBox<String> parkList;
    
@@ -51,9 +53,7 @@ class addNotes extends JPanel {
 		lblAddNote.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblAddNote.setHorizontalAlignment(SwingConstants.CENTER);
 		top.add(lblAddNote);
-		
-		
-		
+				
 		//middle of borderlayout
 		JPanel middle = new JPanel();
 		add(middle, BorderLayout.CENTER);
@@ -65,12 +65,13 @@ class addNotes extends JPanel {
 		lblNewLabel_1.setBounds(340, 35, 112, 39);
 		middle.add(lblNewLabel_1);
 		
-		JComboBox selectPark = new JComboBox<String>(parkList);
+		JComboBox selectPark = new JComboBox();
 		selectPark.setBounds(531, 37, 141, 39);
 		selectPark.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		selectPark.setPreferredSize(new Dimension(110, 35));
 		selectPark.setMinimumSize(new Dimension(11000, 20));
 		selectPark.setBackground(SystemColor.inactiveCaptionBorder);
+		selectPark.setModel(new DefaultComboBoxModel(new String[] {"Carnegie", "Clay Pit", "Heber Dunes", "Hollister Hills", "Hungry Valley", "Oceano Dunes", "Ocohillo Wells", "Prairie City"}));
 		middle.add(selectPark);
 		
 		//enter notes label and text area
@@ -80,22 +81,41 @@ class addNotes extends JPanel {
 		lblEnterNote.setBounds(437, 343, 141, 39);
 		middle.add(lblEnterNote);
 		
-		JTextArea notesEntered = new JTextArea();
-		notesEntered.setBounds(316, 393, 378, 82);
-		middle.add(notesEntered);
+		JTextArea noteEntered = new JTextArea();
+		noteEntered.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		noteEntered.setBounds(316, 393, 378, 82);
+		middle.add(noteEntered);
 		
 		//enter date label and text area
 		JLabel lblEnterDate = new JLabel("Enter Date:");
 		lblEnterDate.setFont(new Font("Tahoma", Font.PLAIN, 23));
-		lblEnterDate.setBounds(368, 179, 117, 49);
+		lblEnterDate.setBounds(260, 196, 117, 31);
 		middle.add(lblEnterDate);
 		
-		searchDate = new JTextField();
-		searchDate.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		searchDate.setBounds(494, 183, 200, 50);
-		middle.add(searchDate);
-		searchDate.setColumns(10);
+		//date combo boxes and labels		
+		JComboBox comboMonth = new JComboBox();
+		comboMonth.setModel(new DefaultComboBoxModel(new String[] {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemember", "December"}));
+		comboMonth.setBounds(427, 196, 125, 31);
+		middle.add(comboMonth);
 		
+		JComboBox comboYear = new JComboBox();
+		comboYear.setModel(new DefaultComboBoxModel(new String[] {"", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004"}));
+		comboYear.setBounds(593, 196, 101, 31);
+		middle.add(comboYear);
+		
+		JLabel month = new JLabel("Month");
+		month.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		month.setHorizontalAlignment(SwingConstants.CENTER);
+		month.setBounds(427, 154, 112, 37);
+		middle.add(month);
+		
+		JLabel Year = new JLabel("Year");
+		Year.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		Year.setHorizontalAlignment(SwingConstants.CENTER);
+		Year.setBounds(608, 160, 68, 25);
+		middle.add(Year);
+		
+				
 		//bottom of border layout
 		JPanel bottom = new JPanel();
 		add(bottom, BorderLayout.SOUTH);
@@ -104,16 +124,16 @@ class addNotes extends JPanel {
 		bottom.add(lblNewLabel);
 		
 		//buttons	
-		JButton back = new JButton("Back");
-		back.setFont(new Font("Tahoma", Font.PLAIN, 29));
-		back.setMaximumSize(new Dimension(89, 55));
-		back.setSize(new Dimension(55, 40));
-		bottom.add(back);
+		JButton btnBack = new JButton("Back");
+		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		btnBack.setMaximumSize(new Dimension(89, 55));
+		btnBack.setSize(new Dimension(55, 40));
+		bottom.add(btnBack);
 				
-		JButton enterNotes = new JButton("Enter Notes");
-		enterNotes.setFont(new Font("Tahoma", Font.PLAIN, 29));
-		bottom.add(enterNotes);
-      
+		JButton btnEnterNotes = new JButton("Enter Notes");
+		btnEnterNotes.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		bottom.add(btnEnterNotes);
+		      
       
    
       
@@ -134,7 +154,7 @@ class addNotes extends JPanel {
                 }
                 if(ae.getSource() == enterNotes){
                      String parkSelection = (String) selectPark.getSelectedItem();
-                     date =searchDate.getText();
+                     date = searchDate.getText();
                      noteEntered = notesEntered.getText();                    
                      /*//add to data base.
                      */
